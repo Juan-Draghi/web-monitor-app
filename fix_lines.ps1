3774d51a86f2ca0113a3f913b3f92723bb2c2dec
Juan-Draghi <143964892+Juan-Draghi@users.noreply.github.com>
2026-03-17 11:26:14 -0300

    Delete fix_lines.ps1

--- fix_lines.ps1
+++
@@ -1,4 +0,0 @@
-$path = "g:\Mi unidad\Colab Notebooks\web_monitor_app\packages.txt"
-$text = [IO.File]::ReadAllText($path)
-$text = $text -replace "`r`n", "`n"
-[IO.File]::WriteAllBytes($path, [System.Text.Encoding]::ASCII.GetBytes($text))
